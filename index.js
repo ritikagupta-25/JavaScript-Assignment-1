92ee4138c035582ac07a140e1917949e70492c08
ritikagupta-25 <ritikagupta7715@gmail.com>
2024-10-18 10:18:01 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,87 @@
+// Question 1...............................
+// Two Numbers (A,B);
+
+// let A = 2;
+// let B = 5;
+
+// let sum = (A+B);
+
+// console.log(sum);
+
+
+
+// Question 2...............................
+
+//  let A = 5;
+//  let B = 3;      
+
+// if (A < 10 && A > B) {
+//     console.log(true);
+// }
+// else {
+//     console.log(false);
+// }
+
+
+//Question 3.....................................
+
+// let A = 12;
+// let B = 20;
+
+
+// condition 1
+
+// if ((A,B)%10) {
+//    console.log(true);
+// } else {
+//    console.log(false);
+// }
+
+// Condition 2
+
+
+// if ((A)%10 != 0 ) {
+//    console.log(true);
+// } else {
+//    console.log(false);
+// }
+
+
+// Question 4......................................
+
+// let N = 4567;
+// let First_Digit = String(N)[0];
+// console.log(First_Digit);
+
+
+// Question 5.......................................
+
+
+// let N = 4567;
+// let Last_Digit = String(N)[3];
+// console.log(Last_Digit);
+
+// Question 6........................................
+
+// let a = 2;
+// let b = 9;
+// let Find_the_remainder =( b % a );
+// console.log(Find_the_remainder );
+
+
+//  Question 7........................................
+// let A = 2 ;
+// let B = 5 ;
+// let Multiply_two_number =(A*B);
+// console.log(Multiply_two_number);
+
+// Question 8..........................................
+// let  A = 50;
+// let  B = 20;
+// let  C = 100;
+// let sum = A + B + C
+
+// console.log(sum);
+
+// let Average = (sum/3);
+// console.log(Average);
